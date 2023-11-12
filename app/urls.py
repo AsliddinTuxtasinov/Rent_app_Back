206122d93d5b11011f5c0a9f3e3e7491a0201ae6
Smarttizim2023 <smarttizim2023@gmail.com>
2023-11-12 23:03:07 +0500

    deploy

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 router = DefaultRouter()
 router.register('protype', ProTypeViewset, basename='protype')
@@ -15,3 +18,7 @@ router.register('income', IncomeViewset, basename='income')
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
