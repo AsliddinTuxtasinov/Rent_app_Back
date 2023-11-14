2e46bd742ffd0b1d28448081f5789a966dc9b5a6
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2023-11-14 15:48:42 +0500

    deloy

--- config/settings.py
+++ config/settings.py
@@ -115,8 +115,11 @@ REST_FRAMEWORK = {
 
 }
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=365),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=365),
+    # "ACCESS_TOKEN_LIFETIME": timedelta(days=1),  # Access token muddati
+    # "REFRESH_TOKEN_LIFETIME": timedelta(days=7),  # Refresh token muddat
+
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -77,6 +77,8 @@ urlpatterns = [
    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+   
+   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
