ffd2de2262d16953fa902e99bfd16118f10eb81a
Azizbek G`ulomov <84550212+AzizbekGulomov2002@users.noreply.github.com>
2023-11-15 09:55:16 +0500

    deploy

--- config/urls.py
+++ config/urls.py
@@ -75,7 +75,7 @@ urlpatterns = [
    
    path('Login/', LoginAPIView.as_view()),
    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-   path('/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
    path('token/access/', TokenVerifyView.as_view(), name='token_access'),
 #    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
    
